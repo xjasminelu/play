30b3e04060062c192d354141242fd204b53a0e47
thefreshduke <scottyshaw1@gmail.com>
2015-09-02 21:17:01 +0100

    Connected IBOutlets and IBActions

---
+++ SwiftSimpleStopwatch/StopwatchViewController.swift
@@ -0,0 +1,45 @@
+//
+//  StopwatchViewController.swift
+//  SwiftSimpleStopwatch
+//
+//  Created by Scotty Shaw on 9/2/15.
+//  Copyright (c) 2015 ___sks6___. All rights reserved.
+//
+
+import UIKit
+
+class StopwatchViewController: UIViewController {
+    
+    @IBOutlet var timeDisplayLabel: UILabel!
+    
+    @IBAction func start(sender: AnyObject) {
+        
+    }
+    
+    @IBAction func stop (sender: AnyObject) {
+        
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
